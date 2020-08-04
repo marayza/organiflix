a4c0dda2869b215643b3571e82873a0ba654765f
unknown <gz.marayza@gmail.com>
2020-08-03 21:18:01 -0300

    Alteracoes A2

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import Menu from './components/Menu'
-import dadosIniciais from './data/dados_iniciais.json';
-import BannerMain from './components/BannerMain';
-import Carousel from './components/Carousel';
-import Footer from './components/Footer';
+import Menu from '../../components/Menu'
+import dadosIniciais from '../../data/dados_iniciais.json';
+import BannerMain from '../../components/BannerMain';
+import Carousel from '../../components/Carousel';
 
-function App() {
+function Home() {
   return (
     <div style={{ background: "#141414"}}>
       <Menu />
+
       <BannerMain
       videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
       url={dadosIniciais.categorias[0].videos[0].url}
@@ -37,4 +37,4 @@ function App() {
   );
 }
 
-export default App;
+export default Home;
