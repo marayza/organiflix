f8efa13b077691e6812423e0fab7154ad26e0657
unknown <gz.marayza@gmail.com>
2020-08-31 20:36:56 -0300

    Heroku

---
+++ index.js
@@ -0,0 +1,24 @@
+''
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Home from './pages/Home/Home';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/video';
+import CadastroCategoria from './pages/cadastro/categoria';
+
+const Page404 = () => (<div> Página 404</div>)
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch> 
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria}/>
+      <Route component={Page404}/>
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
